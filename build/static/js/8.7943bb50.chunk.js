ae298db95bc126d2f5c1017faec5fa77dec20c96
aman jain <amanjain@amans-MacBook-Pro.local>
2022-08-03 13:35:35 +0530

    new build

---
+++ build/static/js/8.7943bb50.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@minimal/minimal-kit-react"]=this["webpackJsonp@minimal/minimal-kit-react"]||[]).push([[8],{1406:function(i,a,t){"use strict";t.r(a);var m=t(1550);a.default=m.a}}]);
+//# sourceMappingURL=8.7943bb50.chunk.js.map
