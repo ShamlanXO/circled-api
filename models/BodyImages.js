243c1eaada8a29f2e00ce1fcfc221c3ac2a9120a
aman jain <amanjain.3331@gmail.com>
2023-02-22 13:57:25 +0530

    added new build

--- models/BodyImages.js
+++ models/BodyImages.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
-
 const objectSchema = new mongoose.Schema(
   {
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
-    images:[{type:String}],
+    images: [{ type: String }],
     weight: { type: Number, default: 0 },
     description: { type: String },
-    
+    title: { type: String },
   },
   { timestamps: true }
 );
