bd141ce0ce2de364322470488e6ab692eeb68d85
aman jain <amanjain.3331@gmail.com>
2023-11-11 21:29:41 +0530

    added new build

---
+++ build/static/js/3.0997eac5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@minimal/minimal-kit-react"]=this["webpackJsonp@minimal/minimal-kit-react"]||[]).push([[3],{1622:function(i,a,t){"use strict";t.r(a);var m=t(1772);a.default=m.a}}]);
+//# sourceMappingURL=3.0997eac5.chunk.js.map
