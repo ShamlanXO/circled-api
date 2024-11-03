e79404a2652e0ff26650b8c451766b770a4ccd05
aman3331 <132757385+aman3331@users.noreply.github.com>
2024-11-04 00:07:09 +0530

    new build

---
+++ build/static/js/141.470abf39.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@minimal/minimal-kit-react"]=this["webpackJsonp@minimal/minimal-kit-react"]||[]).push([[141],{999:function(i,a,t){"use strict";t.r(a);var m=t(1688);a.default=m.a}}]);
+//# sourceMappingURL=141.470abf39.chunk.js.map
